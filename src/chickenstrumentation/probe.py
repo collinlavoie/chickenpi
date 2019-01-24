8495e8a56466db153883cc75f479620459f8d881
Collin Lavoie <github@collinlavoie.com>
2019-01-24 01:27:58 -0500

    Review probe tests

--- src/chickenstrumentation/probe.py
+++ src/chickenstrumentation/probe.py
@@ -1,18 +1,21 @@
 from subprocess import check_output
 import urllib2
 import json
+import re
 
 class Reader(object):
 
     PROBE_BIN="../bin/get_temp.sh"
+    SAMPLE_PATTERN=r'\d{4}(-\d\d){2} (\d\d:){2}\d\d,[0-9a-fA-F]{2}-[0-9a-fA-F]{12},\d\.\d\d'
 
     @classmethod
     def get_data(cls):
         probe_data = cls.read_probes()
         data = []
         for sample in probe_data.splitlines():
-            ( time, sensor, temp ) = sample.split(',')
-            data.append({'sensor': sensor, 'time':time, 'temp':temp})
+            if re.match(cls.SAMPLE_PATTERN, sample):
+                ( time, sensor, temp ) = sample.split(',')
+                data.append({'sensor': sensor, 'time':time, 'temp':temp})
         return data
 
     @classmethod
@@ -21,24 +24,3 @@ class Reader(object):
             return check_output([cls.PROBE_BIN])
         except:
             return ""
-
-class WebReader(Reader):
-
-    @classmethod
-    def get_web_resource(cls):
-        content = urllib2.urlopen("http://chickenstrumentation/1h").read()
-        # get data for desired probe
-        data = [x for x in json.loads(content)]
-        return data
-
-    @classmethod
-    def get_data(cls):
-        return cls.read_probes()
-
-    @classmethod
-    def read_probes(cls):
-         return cls.get_web_resource()
-#        try:
-#        except:
-#            return []
-
--- tests/features/probe.feature
+++ tests/features/probe.feature
@@ -1,12 +1,24 @@
 Feature: Temperature probe
     An interface for obtainin temperature from 2-wire probes
 
-Scenario: Obtaining temperature from local probes
-    Given I have a probe
-    When I obtain the temperature reading
-    Then I should obtain the temperature
+Scenario: Obtaining temperature from the probes
+    Given I have a mock probe
+    And I obtain the reading:
+        2019-01-13 22:30:01,28-0000079ff834,7.125
+    Then I obtain the following data:
+        [{'sensor': u'28-0000079ff834', 'temp': u'7.125', 'time': u'2019-01-13 22:30:01'}]
 
-Scenario: Obtaining temperature from web resource
-    Given I obtain data from a web resource
-    When I obtain the temperature reading
-    Then I should obtain the temperature
+Scenario: Obtaining bad data from the probes
+    Given I have a mock probe
+    And I obtain the reading:
+        xxxx-01-13 22:30:01,28-0000079ff834,7.125
+    Then I obtain the following data:
+        []
+    
+Scenario: Obtaining good and bad data from the probes
+    Given I have a mock probe
+    And I obtain the reading:
+        xxxx-01-13 22:30:01,28-0000079ff834,7.125
+        2019-01-13 22:30:01,28-0000079ff834,7.125
+    Then I obtain the following data:
+        [{'sensor': u'28-0000079ff834', 'temp': u'7.125', 'time': u'2019-01-13 22:30:01'}]
